e9350cfa1d602d75342af05b45b0a2c5fc0e7184
Erdal256 <erdall.2693@gmail.com>
2021-01-30 14:30:04 +0300

    Add project files.

---
+++ Interfaces/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        interface IPersonManager
+        {
+            //unimplemented operation denir. interface oldugu zaman unimplemented olmakta
+            void Add();
+        }
+        //inherist - class --------- implements - interface
+
+        class CustomerManager : IPersonManager
+        {
+            public void Add()
+            {
+                //MÜŞTERİ EKLEME KODLARI
+                Console.WriteLine("Müşteri eklendi");
+            }
+        }
+        class EmployeeManager : IPersonManager
+        {
+            public void Add()
+            {
+                //Personel EKLEME KODLARI
+                Console.WriteLine("Personel eklendi");
+            }
+        }
+    }
+}
